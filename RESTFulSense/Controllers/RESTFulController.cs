87c4ffed615421bc178606c512a01651846ef4ed
Hassan Habib <hassanhabib@live.com>
2020-10-17 11:28:56 -0700

    Code Rub

--- RESTFulSense/Controllers/RESTFulController.cs
+++ RESTFulSense/Controllers/RESTFulController.cs
@@ -9,134 +9,134 @@ using RESTFulSense.Models;
 
 namespace RESTFulSense.Controllers
 {
-	public class RESTFulController : ControllerBase, IRESTFulController
-	{
-		[NonAction]
-		public BadGatewayObjectResult BadGateway(object value) =>
-			new BadGatewayObjectResult(value);
+    public class RESTFulController : ControllerBase, IRESTFulController
+    {
+        [NonAction]
+        public BadGatewayObjectResult BadGateway(object value) =>
+            new BadGatewayObjectResult(value);
 
-		[NonAction]
-		public ExpectationFailedObjectResult ExpectationFailed(object value) =>
-			new ExpectationFailedObjectResult(value);
+        [NonAction]
+        public ExpectationFailedObjectResult ExpectationFailed(object value) =>
+            new ExpectationFailedObjectResult(value);
 
-		[NonAction]
-		public FailedDependencyObjectResult FailedDependency(object value) =>
-			new FailedDependencyObjectResult(value);
+        [NonAction]
+        public FailedDependencyObjectResult FailedDependency(object value) =>
+            new FailedDependencyObjectResult(value);
 
-		[NonAction]
-		public GatewayTimeoutObjectResult GatewayTimeout(object value) =>
-			new GatewayTimeoutObjectResult(value);
+        [NonAction]
+        public GatewayTimeoutObjectResult GatewayTimeout(object value) =>
+            new GatewayTimeoutObjectResult(value);
 
-		[NonAction]
-		public GoneObjectResult Gone(object value) =>
-			new GoneObjectResult(value);
+        [NonAction]
+        public GoneObjectResult Gone(object value) =>
+            new GoneObjectResult(value);
 
-		[NonAction]
-		public HttpVersionNotSupportedObjectResult HttpVersionNotSupported(object value) =>
-			new HttpVersionNotSupportedObjectResult(value);
+        [NonAction]
+        public HttpVersionNotSupportedObjectResult HttpVersionNotSupported(object value) =>
+            new HttpVersionNotSupportedObjectResult(value);
 
-		[NonAction]
-		public InsufficientStorageObjectResult InsufficientStorage(object value) =>
-			new InsufficientStorageObjectResult(value);
+        [NonAction]
+        public InsufficientStorageObjectResult InsufficientStorage(object value) =>
+            new InsufficientStorageObjectResult(value);
 
-		[NonAction]
-		public InternalServerErrorObjectResult InternalServerError(object value) =>
-			new InternalServerErrorObjectResult(value);
+        [NonAction]
+        public InternalServerErrorObjectResult InternalServerError(object value) =>
+            new InternalServerErrorObjectResult(value);
 
-		[NonAction]
-		public LengthRequiredObjectResult LengthRequired(object value) =>
-			new LengthRequiredObjectResult(value);
+        [NonAction]
+        public LengthRequiredObjectResult LengthRequired(object value) =>
+            new LengthRequiredObjectResult(value);
 
-		[NonAction]
-		public LockedObjectResult Locked(object value) =>
-			new LockedObjectResult(value);
+        [NonAction]
+        public LockedObjectResult Locked(object value) =>
+            new LockedObjectResult(value);
 
-		[NonAction]
-		public LoopDetectedObjectResult LoopDetected(object value) =>
-			new LoopDetectedObjectResult(value);
+        [NonAction]
+        public LoopDetectedObjectResult LoopDetected(object value) =>
+            new LoopDetectedObjectResult(value);
 
-		[NonAction]
-		public MethodNotAllowedObjectResult MethodNotAllowed(object value) =>
-			new MethodNotAllowedObjectResult(value);
+        [NonAction]
+        public MethodNotAllowedObjectResult MethodNotAllowed(object value) =>
+            new MethodNotAllowedObjectResult(value);
 
-		[NonAction]
-		public MisdirectedRequestObjectResult MisdirectedRequest(object value) =>
-			new MisdirectedRequestObjectResult(value);
+        [NonAction]
+        public MisdirectedRequestObjectResult MisdirectedRequest(object value) =>
+            new MisdirectedRequestObjectResult(value);
 
-		[NonAction]
-		public NetworkAuthenticationRequiredObjectResult NetworkAuthenticationRequired(object value) =>
-			new NetworkAuthenticationRequiredObjectResult(value);
+        [NonAction]
+        public NetworkAuthenticationRequiredObjectResult NetworkAuthenticationRequired(object value) =>
+            new NetworkAuthenticationRequiredObjectResult(value);
 
-		[NonAction]
-		public NotAcceptableObjectResult NotAcceptable(object value) =>
-			new NotAcceptableObjectResult(value);
+        [NonAction]
+        public NotAcceptableObjectResult NotAcceptable(object value) =>
+            new NotAcceptableObjectResult(value);
 
-		[NonAction]
-		public NotExtendedObjectResult NotExtended(object value) =>
-			new NotExtendedObjectResult(value);
+        [NonAction]
+        public NotExtendedObjectResult NotExtended(object value) =>
+            new NotExtendedObjectResult(value);
 
-		[NonAction]
-		public NotImplementedObjectResult NotImplemented(object value) =>
-			new NotImplementedObjectResult(value);
+        [NonAction]
+        public NotImplementedObjectResult NotImplemented(object value) =>
+            new NotImplementedObjectResult(value);
 
-		[NonAction]
-		public PaymentRequiredObjectResult PaymentRequired(object value) =>
-			new PaymentRequiredObjectResult(value);
+        [NonAction]
+        public PaymentRequiredObjectResult PaymentRequired(object value) =>
+            new PaymentRequiredObjectResult(value);
 
-		[NonAction]
-		public PreconditionFailedObjectResult PreconditionFailed(object value) =>
-			new PreconditionFailedObjectResult(value);
+        [NonAction]
+        public PreconditionFailedObjectResult PreconditionFailed(object value) =>
+            new PreconditionFailedObjectResult(value);
 
-		[NonAction]
-		public PreconditionRequiredObjectResult PreconditionRequired(object value) =>
-			new PreconditionRequiredObjectResult(value);
+        [NonAction]
+        public PreconditionRequiredObjectResult PreconditionRequired(object value) =>
+            new PreconditionRequiredObjectResult(value);
 
-		[NonAction]
-		public ProxyAuthenticationRequiredObjectResult ProxyAuthenticationRequired(object value) =>
-			new ProxyAuthenticationRequiredObjectResult(value);
+        [NonAction]
+        public ProxyAuthenticationRequiredObjectResult ProxyAuthenticationRequired(object value) =>
+            new ProxyAuthenticationRequiredObjectResult(value);
 
-		[NonAction]
-		public RequestedRangeNotSatisfiableObjectResult RequestedRangeNotSatisfiable(object value) =>
-			new RequestedRangeNotSatisfiableObjectResult(value);
+        [NonAction]
+        public RequestedRangeNotSatisfiableObjectResult RequestedRangeNotSatisfiable(object value) =>
+            new RequestedRangeNotSatisfiableObjectResult(value);
 
-		[NonAction]
-		public RequestEntityTooLargeObjectResult RequestEntityTooLarge(object value) =>
-			new RequestEntityTooLargeObjectResult(value);
+        [NonAction]
+        public RequestEntityTooLargeObjectResult RequestEntityTooLarge(object value) =>
+            new RequestEntityTooLargeObjectResult(value);
 
-		[NonAction]
-		public RequestHeaderFieldsTooLargeObjectResult RequestHeaderFieldsTooLarge(object value) =>
-			new RequestHeaderFieldsTooLargeObjectResult(value);
+        [NonAction]
+        public RequestHeaderFieldsTooLargeObjectResult RequestHeaderFieldsTooLarge(object value) =>
+            new RequestHeaderFieldsTooLargeObjectResult(value);
 
-		[NonAction]
-		public RequestTimeoutObjectResult RequestTimeout(object value) =>
-			new RequestTimeoutObjectResult(value);
+        [NonAction]
+        public RequestTimeoutObjectResult RequestTimeout(object value) =>
+            new RequestTimeoutObjectResult(value);
 
-		[NonAction]
-		public RequestUriTooLongObjectResult RequestUriTooLong(object value) =>
-			new RequestUriTooLongObjectResult(value);
+        [NonAction]
+        public RequestUriTooLongObjectResult RequestUriTooLong(object value) =>
+            new RequestUriTooLongObjectResult(value);
 
-		[NonAction]
-		public ServiceUnavailableObjectResult ServiceUnavailable(object value) =>
-			new ServiceUnavailableObjectResult(value);
+        [NonAction]
+        public ServiceUnavailableObjectResult ServiceUnavailable(object value) =>
+            new ServiceUnavailableObjectResult(value);
 
-		[NonAction]
-		public TooManyRequestsObjectResult TooManyRequests(object value) =>
-			new TooManyRequestsObjectResult(value);
+        [NonAction]
+        public TooManyRequestsObjectResult TooManyRequests(object value) =>
+            new TooManyRequestsObjectResult(value);
 
-		[NonAction]
-		public UnavailableForLegalReasonsObjectResult UnavailableForLegalReasons(object value) =>
-			new UnavailableForLegalReasonsObjectResult(value);
+        [NonAction]
+        public UnavailableForLegalReasonsObjectResult UnavailableForLegalReasons(object value) =>
+            new UnavailableForLegalReasonsObjectResult(value);
 
-		[NonAction]
-		public UnsupportedMediaTypeObjectResult UnsupportedMediaTypeObjectResult(object value) =>
-			new UnsupportedMediaTypeObjectResult(value);
+        [NonAction]
+        public UnsupportedMediaTypeObjectResult UnsupportedMediaTypeObjectResult(object value) =>
+            new UnsupportedMediaTypeObjectResult(value);
 
-		[NonAction]
-		public UpgradeRequiredObjectResult UpgradeRequired(object value) =>
-			new UpgradeRequiredObjectResult(value);
+        [NonAction]
+        public UpgradeRequiredObjectResult UpgradeRequired(object value) =>
+            new UpgradeRequiredObjectResult(value);
 
-		[NonAction]
-		public VariantAlsoNegotiatesObjectResult VariantAlsoNegotiates(object value) =>
-			new VariantAlsoNegotiatesObjectResult(value);
-	}
+        [NonAction]
+        public VariantAlsoNegotiatesObjectResult VariantAlsoNegotiates(object value) =>
+            new VariantAlsoNegotiatesObjectResult(value);
+    }
 }
