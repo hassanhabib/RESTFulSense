c1c7b544ba76d0c350e265515dda5c6e24cc65fa
Hassan Habib <harezk@microsoft.com>
2021-09-06 07:43:59 -0700

    ShouldReturnValidationProblemDetailOnLocked -> FAIL

--- RESTFulSense.Tests/Controllers/RESTFulControllerTests.ProblemDetails.cs
+++ RESTFulSense.Tests/Controllers/RESTFulControllerTests.ProblemDetails.cs
@@ -756,6 +756,43 @@ namespace RESTFulSense.Tests.Controllers
                 .BeEquivalentTo(expectedUnprocessableEntityObjectResult);
         }
 
+        [Fact]
+        public void ShouldReturnValidationProblemDetailOnLocked()
+        {
+            // given
+            Dictionary<string, List<string>> randomDictionary =
+                CreateRandomDictionary();
+
+            var inputException = new Exception();
+
+            var expectedProblemDetail = new ValidationProblemDetails
+            {
+                Status = StatusCodes.Status423Locked,
+                Type = "https://tools.ietf.org/html/rfc4918#section-11.3",
+                Title = inputException.Message,
+            };
+
+            var expectedLockedObjectResult =
+                new LockedObjectResult(expectedProblemDetail);
+
+            foreach (KeyValuePair<string, List<string>> item in randomDictionary)
+            {
+                inputException.Data.Add(item.Key, item.Value);
+
+                expectedProblemDetail.Errors.Add(
+                    key: item.Key,
+                    value: item.Value.ToArray());
+            }
+
+            // when
+            LockedObjectResult lockedObjectResult =
+                this.restfulController.Locked(inputException);
+
+            // then
+            lockedObjectResult.Should()
+                .BeEquivalentTo(expectedLockedObjectResult);
+        }
+
         public static Dictionary<string, List<string>> CreateRandomDictionary()
         {
             var filler = new Filler<Dictionary<string, List<string>>>();
