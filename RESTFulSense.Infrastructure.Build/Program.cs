d1e76cb700d72af47c2976de5f1cbd81136256ed
Shri Humrudha Jagathisun <shrija@microsoft.com>
2021-10-12 12:37:59 -0700

    INFRA: Setup Build Project

---
+++ RESTFulSense.Infrastructure.Build/Program.cs
@@ -0,0 +1,84 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+namespace RESTFulSense.Infrastructure.Build
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var adotNetClient = new ADotNetClient();
+
+            var githubPipeline = new GithubPipeline
+            {
+                Name = ".Net",
+
+                OnEvents = new Events
+                {
+                    Push = new PushEvent
+                    {
+                        Branches = new string[] { "master" }
+                    },
+                    PullRequest = new PullRequestEvent
+                    {
+                        Branches = new string[] { "master" }
+                    }
+                },
+
+                Jobs = new Jobs
+                {
+                    Build = new BuildJob
+                    {
+                        RunsOn = BuildMachines.Windows2019,
+
+                        Steps = new List<GithubTask>
+                        {
+                            new CheckoutTaskV2
+                            {
+                                Name = "Check Out"
+                            },
+
+                            new SetupDotNetTaskV1
+                            {
+                                Name = "Setup .Net",
+
+                                TargetDotNetVersion = new TargetDotNetVersion
+                                {
+                                    DotNetVersion = "6.0.100-rc.1.21463.6",
+                                    IncludePrerelease = true
+                                }
+                            },
+
+                            new RestoreTask
+                            {
+                                Name = "Restore"
+                            },
+
+                            new DotNetBuildTask
+                            {
+                                Name = "Build"
+                            },
+
+                            new TestTask
+                            {
+                                Name = "Test"
+                            }
+                        }
+                    }
+                }
+            };
+
+            adotNetClient.SerializeAndWriteToFile(githubPipeline, "../../../../.github/workflows/dotnet.yml");
+        }
+    }
+}
