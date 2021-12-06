17a0ecc2ce68dd5b1ac7cb5c601fd41fbdfd3e6d
Brian Parker <brian.parker@hotmail.com.au>
2021-12-07 01:33:19 +1100

    CODE RUB: Web assembly client library

---
+++ RESTFulSense.WebAssembly.Tests/Services/ValidationServiceTests.cs
@@ -0,0 +1,35 @@
+// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Tynamix.ObjectFiller;
+
+namespace RESTFulSense.WebAssembly.Tests.Services
+{
+    public partial class ValidationServiceTests
+    {
+        private HttpResponseMessage CreateHttpResponseMessage(HttpStatusCode statusCode, string content)
+        {
+            return new HttpResponseMessage(statusCode)
+            {
+                Content = CreateStreamContent(content)
+            };
+        }
+
+        private string GetRandomContent() => new MnemonicString().GetValue();
+
+        private StreamContent CreateStreamContent(string content)
+        {
+            byte[] contentBytes = Encoding.ASCII.GetBytes(content);
+            var stream = new MemoryStream(contentBytes);
+
+            return new StreamContent(stream);
+        }
+    }
+}
--- RESTFulSense.WebAssembly/Models/ValidationProblemDetails.cs
+++ RESTFulSense.WebAssembly/Models/ValidationProblemDetails.cs
@@ -4,16 +4,19 @@
 // See License.txt in the project root for license information.
 // ---------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
-using System.Text.Json.Serialization;
 
 namespace RESTFulSense.WebAssembly.Exceptions
 {
     public class ValidationProblemDetails
     {
-        [JsonPropertyName("title")]
-        public string Title { get; set; }
+        public ValidationProblemDetails() =>
+            Data = new Dictionary<string, string[]>(StringComparer.Ordinal);
 
-        public IDictionary<string, string[]> Errors { get; set; }
+        public string Title { get; set; }
+        public IDictionary<string, string[]> Errors => Data;
+        public Dictionary<string, string[]> Data { get; set; }
+        public string Type { get; set; }
     }
 }
