21b045ed29f221ed3349b0d1f51f9267084a86a1
Brian Parker <brian.parker@hotmail.com.au>
2021-12-09 14:36:26 +1100

    MINOR FIX: API Http Client Factory

---
+++ RESTFulSense.WebAssembly.Tests/Services/RESTFulApiClientFactoryTests.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Net.Http;
+using Moq;
+using RESTFulSense.WebAssembly.Services;
+using Tynamix.ObjectFiller;
+
+namespace RESTFulSense.WebAssembly.Tests.Services
+{
+    public partial class RESTFulApiClientFactoryTests
+    {
+        private readonly Mock<IHttpClientFactory> httpClientFactoryMock;
+        private readonly RESTFulApiClientFactory rESTFulApiClientFactory;
+
+        public RESTFulApiClientFactoryTests()
+        {
+            this.httpClientFactoryMock =
+                new Mock<IHttpClientFactory>();
+
+            this.rESTFulApiClientFactory =
+                new RESTFulApiClientFactory(httpClientFactoryMock.Object);
+        }
+
+        private static string GetRandomClientName() =>
+           new MnemonicString().GetValue();
+    }
+}
---
+++ RESTFulSense.WebAssembly/Services/RESTFulApiClientFactoryExtensions.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using Microsoft.Extensions.Options;
+using RESTFulSense.WebAssembly.Clients;
+
+namespace RESTFulSense.WebAssembly.Services
+{
+    public static class RESTFulApiClientFactoryExtensions
+    {
+        public static RESTFulApiFactoryClient CreateClient(this RESTFulApiClientFactory factory)
+        {
+            if (factory == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(factory));
+            }
+
+            return factory.CreateClient(Options.DefaultName);
+        }
+    }
+}
