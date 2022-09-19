ae3467f728ec27d1818aec7355ebc69967a86821
Christo du Toit <christo@dutoit.co.uk>
2022-09-19 16:58:39 +0100

    MAJORFIX: Ensuring errors is serialized as per serialization policy defined.

--- RESTFulSense.Tests/Controllers/RESTFulControllerTests.cs
+++ RESTFulSense.Tests/Controllers/RESTFulControllerTests.cs
@@ -4,6 +4,11 @@
 // See License.txt in the project root for license information.
 // ---------------------------------------------------------------
 
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
 using RESTFulSense.Controllers;
 using Tynamix.ObjectFiller;
 
@@ -14,7 +19,64 @@ namespace RESTFulSense.Tests.Controllers
         private readonly RESTFulController restfulController;
 
         public RESTFulControllerTests() =>
-            this.restfulController = new RESTFulController();
+            this.restfulController = new RESTFulController(new JsonSerializerOptions());
+
+        public static IEnumerable<object[]> SerializationCases()
+        {
+            JsonSerializerOptions jsonSerializerSettingsPascal = new JsonSerializerOptions();
+
+            JsonSerializerOptions jsonSerializerSettingsCamel = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+
+            return new List<object[]>
+            {
+                new object[] { jsonSerializerSettingsPascal },
+                new object[] { jsonSerializerSettingsCamel }
+            };
+        }
+
+        private void SetupInputAndExpectedCriteria(
+            Dictionary<string, List<string>> randomDictionary,
+            Exception inputException,
+            ValidationProblemDetails expectedProblemDetail)
+        {
+            this.SetupInputAndExpectedCriteria(
+                randomDictionary,
+                inputException,
+                expectedProblemDetail,
+                jsonSerializerOptions: new JsonSerializerOptions());
+        }
+
+        private void SetupInputAndExpectedCriteria(
+            Dictionary<string, List<string>> randomDictionary,
+            Exception inputException,
+            ValidationProblemDetails expectedProblemDetail,
+            JsonSerializerOptions jsonSerializerOptions)
+        {
+            foreach (KeyValuePair<string, List<string>> item in randomDictionary)
+            {
+                inputException.Data.Add(item.Key, item.Value);
+
+                string serializedKey = ApplySerialization(
+                    error: new DictionaryEntry(item.Key, item.Value),
+                    jsonSerializerOptions: jsonSerializerOptions);
+
+                expectedProblemDetail.Errors.Add(
+                    key: serializedKey,
+                    value: item.Value.ToArray());
+            }
+        }
+
+        private static string ApplySerialization(DictionaryEntry error, JsonSerializerOptions jsonSerializerOptions)
+        {
+            return jsonSerializerOptions.PropertyNamingPolicy == JsonNamingPolicy.CamelCase
+                ? error.Key.ToString().Substring(0, 1).ToLower() + error.Key.ToString().Substring(1)
+                : error.Key.ToString().Substring(0, 1).ToUpper() + error.Key.ToString().Substring(1);
+        }
 
         private static string GetRandomMessage() =>
             new MnemonicString().GetValue();
