6f516e9d97672e642aa7a7ab92d17847255bc1dd
Brian Parker <brian.parker@hotmail.com.au>
2023-04-13 17:33:42 +1000

    BROKERS: WebAssemby Reflections Get Properties

---
+++ RESTFulSense.WebAssembly/Brokers/Reflections/IReflectionBroker.Properties.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace RESTFulSense.WebAssembly.Brokers.Reflections
+{
+    public partial interface IReflectionBroker
+    {
+        IEnumerable<PropertyInfo> GetProperties(object @object);
+    }
+}
