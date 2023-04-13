deb0a5ee18b02ba5ff62342945c94653f5e155a1
Brian Parker <brian.parker@hotmail.com.au>
2023-04-13 22:08:33 +1000

    BROKERS Reflection Get Stream Property Value

---
+++ RESTFulSense/Brokers/Reflections/ReflectionBroker.StreamPropertyValues.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.IO;
+using System.Reflection;
+
+namespace RESTFulSense.Brokers.Reflections
+{
+    internal partial class ReflectionBroker
+    {
+        public Stream GetStreamPropertyValue(object @object, PropertyInfo property) =>
+            this.GetPropertyValue<Stream>(@object, property);
+    }
+}
