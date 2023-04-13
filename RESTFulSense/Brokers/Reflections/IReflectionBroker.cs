de1ffbf181cd7b5d006aabb83589e96defe0efd1
Brian Parker <brian.parker@hotmail.com.au>
2023-04-13 13:35:54 +1000

    BROKERS: Reflections

---
+++ RESTFulSense/Brokers/Reflections/IReflectionBroker.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace RESTFulSense.Brokers.Reflections
+{
+    internal interface IReflectionBroker
+    {
+        IEnumerable<PropertyInfo> GetProperties(object @object);
+        TAttribute GetCustomAttribute<TAttribute>(PropertyInfo property) where TAttribute : Attribute;
+        TResult GetPropertyValue<TResult>(object @object, PropertyInfo property);
+    }
+}
---
+++ RESTFulSense/Brokers/Reflections/ReflectionBroker.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace RESTFulSense.Brokers.Reflections
+{
+    internal class ReflectionBroker : IReflectionBroker
+    {
+        public IEnumerable<PropertyInfo> GetProperties(object @object) =>
+            @object.GetType().GetProperties();
+
+        public TAttribute GetCustomAttribute<TAttribute>(PropertyInfo property)
+            where TAttribute : Attribute =>
+            property.GetCustomAttribute<TAttribute>();
+
+        public TResult GetPropertyValue<TResult>(object @object, PropertyInfo property) =>
+            (TResult)property.GetValue(@object);
+    }
+}
