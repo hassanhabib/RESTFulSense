c05543c15ec3e488c183a7701aed8d40aa1708b7
Brian Parker <brian.parker@hotmail.com.au>
2023-04-15 04:58:49 +1000

    ShouldThrowServiceExceptionOnRetrievePropertiesIfServiceErrorOccurs -> FAIL

--- RESTFulSense.Tests/Services/Foundations/Properties/PropertyServiceTests.Validations.Retrieve.cs
+++ RESTFulSense.Tests/Services/Foundations/Properties/PropertyServiceTests.Validations.Retrieve.cs
@@ -2,8 +2,13 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Moq;
+using RESTFulSense.Models.Foundations.Properties;
 using RESTFulSense.Models.Foundations.Properties.Exceptions;
 using Xunit;
 
@@ -32,6 +37,42 @@ namespace RESTFulSense.Tests.Services.Foundations.Properties
 
             this.reflectionBrokerMock.Verify(reflectionBroker =>
                 reflectionBroker.GetPropertyValues(It.IsAny<object>()), Times.Never);
+
+            this.reflectionBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void ShouldThrowServiceExceptionOnRetrievePropertiesIfServiceErrorOccurs()
+        {
+            // given
+            object someObject = new object();
+            
+            var serviceException = new Exception();
+
+            var failedPropertyServiceException =
+                new FailedPropertyServiceException(serviceException);
+
+            var expectedPropertyServiceException =
+                new PropertyServiceException(
+                    failedPropertyServiceException);
+
+            this.reflectionBrokerMock.Setup(broker => broker.GetPropertyValues(
+                It.IsAny<object>()))
+                    .Throws(serviceException);
+
+            // when
+
+            PropertyServiceException actualPropertyServiceException =
+                  Assert.Throws<PropertyServiceException>(() => this.propertyService.RetrieveProperties(someObject));
+
+            // then
+            actualPropertyServiceException.Should().BeEquivalentTo(
+                expectedPropertyServiceException);
+
+            this.reflectionBrokerMock.Verify(broker =>
+                broker.GetPropertyValues(
+                    It.IsAny<object>()),
+                        Times.Once);
 
             this.reflectionBrokerMock.VerifyNoOtherCalls();
         }
---
+++ RESTFulSense/Models/Foundations/Properties/Exceptions/PropertyServiceException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace RESTFulSense.Models.Foundations.Properties.Exceptions
+{
+    public class PropertyServiceException : Xeption
+    {
+        public PropertyServiceException(Xeption innerException)
+            : base(message: "Property service error occurred, contact support.",
+                  innerException)
+        { }
+    }
+}
