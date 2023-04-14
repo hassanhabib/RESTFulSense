c1b13ec7530776798b57f68541ac816625ddc9f4
Brian Parker <brian.parker@hotmail.com.au>
2023-04-15 09:25:57 +1000

    ShouldThrowServiceExceptionOnRetrieveStreamContentIfServiceErrorOccurs -> FAIL

---
+++ RESTFulSense.Tests/Services/Foundations/StreamContents/StreamContentServiceTests.Exceptions.Retrieve.cs
@@ -0,0 +1,52 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using FluentAssertions;
+using Moq;
+using RESTFulSense.Models.Foundations.StreamContents.Exceptions;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace RESTFulSense.Tests.Services.Foundations.StreamContents
+{
+    public partial class StreamContentServiceTests
+    {
+        [Fact]
+        public void ShouldThrowServiceExceptionOnRetrieveStreamContentIfServiceErrorOccurs()
+        {
+            // given
+            PropertyInfo somePropertyInfo = new Mock<PropertyInfo>().Object;
+
+            var serviceException = new Exception();
+
+            var failedStreamContentServiceException =
+                new FailedStreamContentServiceException(serviceException);
+
+            var expectedStreamContentServiceException =
+                new StreamContentServiceException(
+                    failedStreamContentServiceException);
+
+            this.reflectionBrokerMock.Setup(broker => broker.GetFileContentStreamAttribute(
+                somePropertyInfo))
+                    .Throws(serviceException);
+
+            // when
+            StreamContentServiceException actualStreamContentServiceException =
+                Assert.Throws<StreamContentServiceException>(() =>
+                    this.streamContentService.RetrieveStreamContent(somePropertyInfo));
+
+            // then
+            actualStreamContentServiceException.Should().BeEquivalentTo(
+                expectedStreamContentServiceException);
+
+            this.reflectionBrokerMock.Verify(broker =>
+                broker.GetFileContentStreamAttribute(
+                    somePropertyInfo),
+                        Times.Once);
+
+            this.reflectionBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ RESTFulSense/Models/Foundations/StreamContents/Exceptions/StreamContentServiceException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace RESTFulSense.Models.Foundations.StreamContents.Exceptions
+{
+    public class StreamContentServiceException : Xeption
+    {
+        public StreamContentServiceException(Xeption innerException)
+            : base(message: "StreamContent service error occurred, contact support.",
+                  innerException)
+        { }
+    }
+}
