0dbf2560a4bfa184b30a78064745e2359697aa6e
Brian Parker <brian.parker@hotmail.com.au>
2023-04-15 08:59:02 +1000

    ShouldThrowValidationExceptionOnRetrieveStreamContentIfPropertyInfoIsNull -> FAIL

---
+++ RESTFulSense/Models/Foundations/StreamContents/Exceptions/StreamContentValidationException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace RESTFulSense.Models.Foundations.StringContents.Exceptions
+{
+    public class StreamContentValidationException : Xeption
+    {
+        public StreamContentValidationException(Xeption innerException)
+            : base(message: "StreamContent validation error occurred, fix errors and try again.",
+                  innerException)
+        { }
+    }
+}
