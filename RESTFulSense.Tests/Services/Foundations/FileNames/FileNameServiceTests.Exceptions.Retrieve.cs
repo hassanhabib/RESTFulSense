73ec06d5283a872dbc2d619ba2dcb3bd2d582a73
Brian Parker <brian.parker@hotmail.com.au>
2023-04-15 14:54:02 +1000

    ShouldThrowServiceExceptionOnRetrieveFileNameIfServiceErrorOccurs -> FAIL

--- RESTFulSense.Tests/Services/Foundations/FileNames/FileNameServiceTests.Exceptions.Retrieve.cs
+++ RESTFulSense.Tests/Services/Foundations/FileNames/FileNameServiceTests.Exceptions.Retrieve.cs
@@ -2,9 +2,52 @@
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
 
+using Moq;
+using RESTFulSense.Models.Foundations.FileNames.Exceptions;
+using System.Reflection;
+using System;
+using Xunit;
+using FluentAssertions;
+
 namespace RESTFulSense.Tests.Services.Foundations.FileNames
 {
     public partial class FileNameServiceTests
     {
+        [Fact]
+        public void ShouldThrowServiceExceptionOnRetrieveFileNameIfServiceErrorOccurs()
+        {
+            // given
+            PropertyInfo somePropertyInfo = new Mock<PropertyInfo>().Object;
+
+            var serviceException = new Exception();
+
+            var failedFileNameServiceException =
+                new FailedFileNameServiceException(serviceException);
+
+            var expectedFileNameServiceException =
+                new FileNameServiceException(
+                    failedFileNameServiceException);
+
+            this.reflectionBrokerMock.Setup(broker => broker.GetFileContentNameAttribute(
+                somePropertyInfo))
+                    .Throws(serviceException);
+
+            // when
+            FileNameServiceException actualFileNameServiceException =
+                Assert.Throws<FileNameServiceException>(() =>
+                    this.fileNameService.RetrieveFileName(somePropertyInfo));
+
+            // then
+            actualFileNameServiceException.Should().BeEquivalentTo(
+                expectedFileNameServiceException);
+
+            this.reflectionBrokerMock.Verify(broker =>
+                broker.GetFileContentNameAttribute(
+                    somePropertyInfo),
+                        Times.Once);
+
+            this.reflectionBrokerMock.VerifyNoOtherCalls();
+        }
+
     }
 }
