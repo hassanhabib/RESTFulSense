a7de9dcaf9c3bd161d686ae6cc76183930c06be0
Brian Parker <brian.parker@hotmail.com.au>
2023-04-20 06:24:47 +1000

    ShouldThrowFormContentOrchestrationDependencyExceptionIfStringContentProcessingDependencyExceptionOccurs -> FAIL

--- RESTFulSense.Tests/Services/Orchestrations/FormContents/FormContentOrchestrationServiceTests.Exceptions.Convert.cs
+++ RESTFulSense.Tests/Services/Orchestrations/FormContents/FormContentOrchestrationServiceTests.Exceptions.Convert.cs
@@ -14,6 +14,7 @@ using RESTFulSense.Models.Orchestrations.FormContents.Exceptions;
 using RESTFulSense.Models.Processings.Properties.Exceptions;
 using RESTFulSense.Models.Processings.StreamContents;
 using RESTFulSense.Models.Processings.StringContents;
+using RESTFulSense.Models.Processings.StringContents.Exceptions;
 using Xunit;
 
 namespace RESTFulSense.Tests.Services.Orchestrations.FormContents
@@ -66,6 +67,67 @@ namespace RESTFulSense.Tests.Services.Orchestrations.FormContents
             this.propertyProcessingServiceMock.Verify(service =>
                 service.RetrieveProperties(inputObject),
                     Times.Once);
+
+            this.propertyProcessingServiceMock.VerifyNoOtherCalls();
+            this.stringContentProcessingServiceMock.VerifyNoOtherCalls();
+            this.streamContentProcessingServiceMock.VerifyNoOtherCalls();
+            this.fileNameProcessingServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void ShouldThrowFormContentOrchestrationDependencyExceptionIfStringContentProcessingDependencyExceptionOccurs()
+        {
+            // given
+            Object someObject = CreateSomeObject();
+            Object inputObject = someObject;
+
+            List<dynamic> randomProperties = GetRandomProperties();
+
+            List<PropertyValue> returnedPropertyValues =
+                randomProperties.Select(CreatePropertyValue).ToList();
+
+            List<NamedStringContent> returnedNamedStringContents
+                = randomProperties.Where(randomProperty => randomProperty.Type == PropertyType.StringContent)
+                     .Select(CreateNamedStringContent).ToList();
+
+            List<NamedStreamContent> returnedNamedStreamContents
+                = randomProperties.Where(randomProperty => randomProperty.Type == PropertyType.StreamContent)
+                     .Select(CreateNamedStreamContent).ToList();
+
+            var nullObjectException = new NullObjectException();
+
+            var stringContentProcessingDependencyException =
+                new StringContentProcessingDependencyException(nullObjectException);
+
+            var expectedFormContentOrchestrationDependencyValidationException =
+                new FormContentOrchestrationDependencyValidationException(stringContentProcessingDependencyException);
+
+            this.propertyProcessingServiceMock.Setup(service =>
+                service.RetrieveProperties(It.IsAny<Object>()))
+                    .Returns(returnedPropertyValues);
+
+            this.stringContentProcessingServiceMock.Setup(service =>
+                service.FilterStringContents(returnedPropertyValues))
+                    .Throws(stringContentProcessingDependencyException);
+
+            // when
+            Func<MultipartFormDataContent> convertToMultipartFormDataContentFunction = () =>
+                this.formContentOrchestrationService.ConvertToMultipartFormDataContent(inputObject);
+
+            FormContentOrchestrationDependencyValidationException actualFormContentOrchestrationDependencyValidationException =
+               Assert.Throws<FormContentOrchestrationDependencyValidationException>(convertToMultipartFormDataContentFunction);
+
+            // then
+            actualFormContentOrchestrationDependencyValidationException.Should()
+                .BeEquivalentTo(expectedFormContentOrchestrationDependencyValidationException);
+
+            this.propertyProcessingServiceMock.Verify(service =>
+                service.RetrieveProperties(inputObject),
+                    Times.Once);
+
+            this.stringContentProcessingServiceMock.Verify(service =>
+                service.FilterStringContents(returnedPropertyValues),
+                     Times.Once);
 
             this.propertyProcessingServiceMock.VerifyNoOtherCalls();
             this.stringContentProcessingServiceMock.VerifyNoOtherCalls();
