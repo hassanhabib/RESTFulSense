f239f516d8dcb299f733deb7c5ac7b0bf4a04422
Brian Parker <brian.parker@hotmail.com.au>
2023-05-03 23:43:48 +1000

    BROKERS: Get Property Value

---
+++ RESTFulSense/Brokers/Values/ValueBroker.cs
@@ -0,0 +1,14 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Reflection;
+
+namespace RESTFulSense.Brokers.Values
+{
+    internal class ValueBroker : IValueBroker
+    {
+        public object GetPropertyValue(object @object, PropertyInfo propertyInfo) =>
+            propertyInfo.GetValue(@object);
+    }
+}
