074f2fad1f6581ed2fed1acc37b2c1e5ea9b898d
Christo du Toit <christo@dutoit.co.uk>
2023-05-03 22:27:39 +0100

    ShouldThrowValidationExceptionOnConvertIfModelIsInvalid -> FAIL

---
+++ RESTFulSense/Models/Coordinations/Forms/Exceptions/FormCoordinationValidationException.cs
@@ -0,0 +1,17 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace RESTFulSense.Models.Coordinations.Forms.Exceptions
+{
+    internal class FormCoordinationValidationException : Xeption
+    {
+        public FormCoordinationValidationException(Xeption innerException)
+            : base(
+                message: "Form coordination validation errors occurred, please try again.",
+                innerException)
+        { }
+    }
+}
