85f566fb7357668c80182adf62324fbc78d5773c
Christo du Toit <christo@dutoit.co.uk>
2023-05-03 21:31:27 +0100

    ShouldBuildFormModel -> FAIL

---
+++ RESTFulSense/Services/Orchestrations/Forms/FormOrchestrationService.cs
@@ -0,0 +1,31 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using RESTFulSense.Models.Orchestrations.Forms;
+using RESTFulSense.Services.Foundations.Attributes;
+using RESTFulSense.Services.Foundations.Forms;
+using RESTFulSense.Services.Foundations.Values;
+
+namespace RESTFulSense.Services.Orchestrations.Forms
+{
+    internal partial class FormOrchestrationService : IFormOrchestrationService
+    {
+        private readonly IAttributeService attributeService;
+        private readonly IValueService valueService;
+        private readonly IFormService formService;
+
+        public FormOrchestrationService(
+            IAttributeService attributeService,
+            IValueService valueService,
+            IFormService formService)
+        {
+            this.attributeService = attributeService;
+            this.valueService = valueService;
+            this.formService = formService;
+        }
+
+        public FormModel BuildFormModel(FormModel formModel) =>
+            throw new System.NotImplementedException();
+    }
+}
---
+++ RESTFulSense/Services/Orchestrations/Forms/IFormOrchestrationService.cs
@@ -0,0 +1,13 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using RESTFulSense.Models.Orchestrations.Forms;
+
+namespace RESTFulSense.Services.Orchestrations.Forms
+{
+    internal interface IFormOrchestrationService
+    {
+        FormModel BuildFormModel(FormModel formModel);
+    }
+}
