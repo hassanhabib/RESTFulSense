645096869c31ec3150b16e0504d9578b4d367a92
Christo du Toit <christo@dutoit.co.uk>
2023-05-03 22:35:29 +0100

    ShouldThrowDependencyValidationExceptionOnConvertIfDependencyValidationErrorOccurs -> PASS

--- RESTFulSense/Services/Coordinations/Forms/FormCoordinationService.Exceptions.cs
+++ RESTFulSense/Services/Coordinations/Forms/FormCoordinationService.Exceptions.cs
@@ -4,6 +4,8 @@
 
 using System.Net.Http;
 using RESTFulSense.Models.Coordinations.Forms.Exceptions;
+using RESTFulSense.Models.Orchestrations.Forms.Exceptions;
+using RESTFulSense.Models.Orchestrations.Properties.Exceptions;
 
 namespace RESTFulSense.Services.Coordinations.Forms
 {
@@ -18,12 +20,40 @@ namespace RESTFulSense.Services.Coordinations.Forms
             {
                 return returningMultipartFormDataContentFunction();
             }
-            catch (NullObjectException nullObjectException)
+            catch (Models.Coordinations.Forms.Exceptions.NullObjectException nullObjectException)
             {
                 var formCoordinationValidationException =
                     new FormCoordinationValidationException(nullObjectException);
 
                 throw formCoordinationValidationException;
+            }
+            catch (FormOrchestrationValidationException formOrchestrationValidationException)
+            {
+                var formCoordinationDependencyValidationException =
+                    new FormCoordinationDependencyValidationException(formOrchestrationValidationException);
+
+                throw formCoordinationDependencyValidationException;
+            }
+            catch (FormOrchestrationDependencyValidationException formOrchestrationDependencyValidationException)
+            {
+                var formCoordinationDependencyValidationException =
+                    new FormCoordinationDependencyValidationException(formOrchestrationDependencyValidationException);
+
+                throw formCoordinationDependencyValidationException;
+            }
+            catch (PropertyOrchestrationValidationException propertyOrchestrationValidationException)
+            {
+                var formCoordinationDependencyValidationException =
+                    new FormCoordinationDependencyValidationException(propertyOrchestrationValidationException);
+
+                throw formCoordinationDependencyValidationException;
+            }
+            catch (PropertyOrchestrationDependencyValidationException propertyOrchestrationDependencyValidationException)
+            {
+                var formCoordinationDependencyValidationException =
+                    new FormCoordinationDependencyValidationException(propertyOrchestrationDependencyValidationException);
+
+                throw formCoordinationDependencyValidationException;
             }
         }
     }
