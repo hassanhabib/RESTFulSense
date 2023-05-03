223467aa7d84144dd49f3a1737025b1bacbc182d
Brian Parker <brian.parker@hotmail.com.au>
2023-05-03 23:47:18 +1000

    BROKERS: Add Multipart Form Content

---
+++ RESTFulSense/Brokers/MultipartFormDataContents/IMultipartFormDataContentBroker.cs
@@ -0,0 +1,39 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.IO;
+using System.Net.Http;
+
+namespace RESTFulSense.Brokers.MultipartFormDataContents
+{
+    internal interface IMultipartFormDataContentBroker
+    {
+        MultipartFormDataContent AddByteContent(
+            MultipartFormDataContent multipartFormDataContent,
+            byte[] content,
+            string name);
+
+        MultipartFormDataContent AddByteContent(
+            MultipartFormDataContent multipartFormDataContent,
+            byte[] content,
+            string name,
+            string fileName);
+
+        MultipartFormDataContent AddStringContent(
+            MultipartFormDataContent multipartFormDataContent,
+            string content,
+            string name);
+
+        MultipartFormDataContent AddStreamContent(
+            MultipartFormDataContent multipartFormDataContent,
+            Stream stream,
+            string name);
+
+        MultipartFormDataContent AddStreamContent(
+            MultipartFormDataContent multipartFormDataContent,
+            Stream content,
+            string name,
+            string fileName);
+    }
+}
