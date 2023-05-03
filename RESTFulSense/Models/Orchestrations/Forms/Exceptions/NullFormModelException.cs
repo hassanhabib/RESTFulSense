3a718d328ea59193a26d46621c0fdcb18c5fc3b6
Christo du Toit <christo@dutoit.co.uk>
2023-05-03 21:39:06 +0100

    ShouldThrowValidationExceptionOnBuildFormModelIfModelIsInvalid -> FAIL

---
+++ RESTFulSense/Models/Orchestrations/Forms/Exceptions/NullFormModelException.cs
@@ -0,0 +1,15 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace RESTFulSense.Models.Orchestrations.Forms.Exceptions
+{
+    internal class NullFormModelException : Xeption
+    {
+        public NullFormModelException()
+            : base(message: "Form model is null. Please correct the errors and try again.")
+        { }
+    }
+}
