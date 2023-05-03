2fc6a1165a4c909e9c0cacf7bc818b5aa8622887
Christo du Toit <christo@dutoit.co.uk>
2023-05-03 22:02:57 +0100

    ShouldThrowDependencyValidationExceptionOnBuildIfDependValidationErrorOccurs -> PASS

--- RESTFulSense/Services/Orchestrations/Forms/FormOrchestrationService.Exceptions.cs
+++ RESTFulSense/Services/Orchestrations/Forms/FormOrchestrationService.Exceptions.cs
@@ -2,6 +2,9 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using RESTFulSense.Models.Foundations.Attributes.Exceptions;
+using RESTFulSense.Models.Foundations.Forms.Exceptions;
+using RESTFulSense.Models.Foundations.Values.Exceptions;
 using RESTFulSense.Models.Orchestrations.Forms;
 using RESTFulSense.Models.Orchestrations.Forms.Exceptions;
 
@@ -23,6 +26,48 @@ namespace RESTFulSense.Services.Orchestrations.Forms
                     new FormOrchestrationValidationException(nullFormModelException);
 
                 throw formOrchestrationValidationException;
+            }
+            catch (AttributeValidationException attributeValidationException)
+            {
+                var formOrchestrationDependencyValidationException =
+                    new FormOrchestrationDependencyValidationException(attributeValidationException);
+
+                throw formOrchestrationDependencyValidationException;
+            }
+            catch (AttributeDependencyValidationException attributeDependencyValidationException)
+            {
+                var formOrchestrationDependencyValidationException =
+                    new FormOrchestrationDependencyValidationException(attributeDependencyValidationException);
+
+                throw formOrchestrationDependencyValidationException;
+            }
+            catch (ValueValidationException valueValidationException)
+            {
+                var formOrchestrationDependencyValidationException =
+                    new FormOrchestrationDependencyValidationException(valueValidationException);
+
+                throw formOrchestrationDependencyValidationException;
+            }
+            catch (ValueDependencyValidationException valueDependencyValidationException)
+            {
+                var formOrchestrationDependencyValidationException =
+                    new FormOrchestrationDependencyValidationException(valueDependencyValidationException);
+
+                throw formOrchestrationDependencyValidationException;
+            }
+            catch (FormValidationException formValidationException)
+            {
+                var formOrchestrationDependencyValidationException =
+                    new FormOrchestrationDependencyValidationException(formValidationException);
+
+                throw formOrchestrationDependencyValidationException;
+            }
+            catch (FormDependencyValidationException formDependencyValidationException)
+            {
+                var formOrchestrationDependencyValidationException =
+                    new FormOrchestrationDependencyValidationException(formDependencyValidationException);
+
+                throw formOrchestrationDependencyValidationException;
             }
         }
     }
