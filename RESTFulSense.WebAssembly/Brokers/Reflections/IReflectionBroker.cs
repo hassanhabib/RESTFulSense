99e9637a3f2f2cf71815efe82ee8500d8e3b3653
Brian Parker <brian.parker@hotmail.com.au>
2023-05-03 23:18:35 +1000

    CODE RUB: Remove Reflection

--- RESTFulSense.WebAssembly/Brokers/Reflections/IReflectionBroker.cs
+++
@@ -1,9 +0,0 @@
-﻿// ---------------------------------------------------------------------------------- 
-// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
-// ----------------------------------------------------------------------------------
-
-namespace RESTFulSense.WebAssembly.Brokers.Reflections
-{
-    internal partial interface IReflectionBroker
-    { }
-}
--- RESTFulSense/Brokers/Reflections/IReflectionBroker.cs
+++
@@ -1,9 +0,0 @@
-﻿// ---------------------------------------------------------------------------------- 
-// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
-// ----------------------------------------------------------------------------------
-
-namespace RESTFulSense.Brokers.Reflections
-{
-    internal partial interface IReflectionBroker
-    { }
-}
