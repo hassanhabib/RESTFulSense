7a3414b84117dda5c4bfbcd544665f8ac38752f2
Brian Parker <brian.parker@hotmail.com.au>
2023-05-03 23:31:21 +1000

    BROKERS: Get Type

---
+++ RESTFulSense/Brokers/Types/ITypeBroker.cs
@@ -0,0 +1,13 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+
+namespace RESTFulSense.Brokers.Types
+{
+    internal interface ITypeBroker
+    {
+        Type GetType(object @object);
+    }
+}
---
+++ RESTFulSense/Brokers/Types/TypeBroker.cs
@@ -0,0 +1,14 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+
+namespace RESTFulSense.Brokers.Types
+{
+    internal class TypeBroker : ITypeBroker
+    {
+        public Type GetType(object @object) =>
+            @object.GetType();
+    }
+}
