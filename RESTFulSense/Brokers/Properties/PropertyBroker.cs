90039e05a7c0933cecc72f63f6817b4e34d1b64d
Brian Parker <brian.parker@hotmail.com.au>
2023-05-03 23:34:13 +1000

    BROKERS: Get Properties

---
+++ RESTFulSense/Brokers/Properties/PropertyBroker.cs
@@ -0,0 +1,15 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+
+namespace RESTFulSense.Brokers.Properties
+{
+    internal class PropertyBroker : IPropertyBroker
+    {
+        public PropertyInfo[] GetProperties(Type type) =>
+           type.GetProperties();
+    }
+}
