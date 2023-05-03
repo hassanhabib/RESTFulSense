c0f9d986aa464c6c2004f3040121afc2022a2631
Brian Parker <brian.parker@hotmail.com.au>
2023-05-04 01:26:06 +1000

    ShouldThrowValidationExceptionOnRetrieveIfPropertyInfoIsNull -> FAIL

---
+++ RESTFulSense.Tests/Services/Foundations/Attributes/AttributeServiceTests.Validations.Retrieve.cs
@@ -0,0 +1,47 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using FluentAssertions;
+using Moq;
+using RESTFulSense.Models.Foundations.Attributes.Exceptions;
+using Xunit;
+
+namespace RESTFulSense.Tests.Services.Foundations.Attributes
+{
+    public partial class AttributeServiceTests
+    {
+        [Fact]
+        public void ShouldThrowValidationExceptionOnRetrieveIfPropertyInfoIsNull()
+        {
+            // given
+            PropertyInfo nullPropertyInfo = CreateNullPropertyInfo();
+
+            var argumentNullException = new ArgumentNullException();
+
+            var nullPropertyInfoException =
+                new NullPropertyInfoException(argumentNullException);
+
+            var expectedAttributeValidationException =
+                new AttributeValidationException(nullPropertyInfoException);
+
+            // when
+            Action retrieveAttributeAction =
+                () => this.attributeService.RetrieveAttribute<TestAttribute>(nullPropertyInfo);
+
+            var actualAttributeValidationException =
+                  Assert.Throws<AttributeValidationException>(retrieveAttributeAction);
+
+            // then
+            actualAttributeValidationException.Should().BeEquivalentTo(expectedAttributeValidationException);
+
+            this.attributeBrokerMock.Verify(broker =>
+                broker.GetPropertyCustomAttribute<TestAttribute>(It.IsAny<PropertyInfo>(), It.IsAny<bool>()),
+                    Times.Never());
+
+            this.attributeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
