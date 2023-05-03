2769d8e8a7832eb0d583f60df810bed93655dc5a
Brian Parker <brian.parker@hotmail.com.au>
2023-05-04 00:48:07 +1000

    ShouldThrowValidationExceptionOnRetrieveTypeIfTypeIsNull -> FAIL

---
+++ RESTFulSense/Services/Properties/PropertyService.Exceptions.cs
@@ -0,0 +1,54 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using RESTFulSense.Models.Foundations.Properties.Exceptions;
+
+namespace RESTFulSense.Services.Properties
+{
+    internal partial class PropertyService
+    {
+        private delegate PropertyInfo[] ReturningPropertiesFunction();
+
+        private PropertyInfo[] TryCatch(ReturningPropertiesFunction returningPropertiesFunction)
+        {
+            return returningPropertiesFunction();
+        }
+
+        private static PropertyDependencyValidationException CreateDependencyValidationException(
+            Exception exception)
+        {
+            var failedPropertyDependencyValidationException =
+                new FailedPropertyDependencyValidationException(exception);
+
+            var propertyDependencyValidationException =
+                new PropertyDependencyValidationException(failedPropertyDependencyValidationException);
+
+            return propertyDependencyValidationException;
+        }
+
+        private static PropertyDependencyException CreateDependencyException(
+            Exception exception)
+        {
+            var failedPropertyDependencyException =
+                new FailedPropertyDependencyException(exception);
+
+            var propertyDependencyException =
+                new PropertyDependencyException(failedPropertyDependencyException);
+
+            return propertyDependencyException;
+        }
+
+        private static PropertyServiceException CreateServiceException(Exception exception)
+        {
+            var failedPropertyServiceException =
+                new FailedPropertyServiceException(exception);
+
+            var propertyServiceException =
+                new PropertyServiceException(failedPropertyServiceException);
+            return propertyServiceException;
+        }
+    }
+}
---
+++ RESTFulSense/Services/Properties/PropertyService.Validations.cs
@@ -0,0 +1,20 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using RESTFulSense.Models.Foundations.Properties.Exceptions;
+
+namespace RESTFulSense.Services.Properties
+{
+    internal partial class PropertyService
+    {
+        private void ValidateTypeIsNotNullOnRetrieveProperties(Type type)
+        {
+            if (type is null)
+            {
+                throw new NullTypeException();
+            }
+        }
+    }
+}
