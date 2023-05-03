f1d327e360b2680ddb6ca914201dc898cea0591d
Brian Parker <brian.parker@hotmail.com.au>
2023-05-04 04:20:42 +1000

    ShouldThrowFormValidationExceptionOnAddStreamContentWithFileNameIfArgumentsInvalid -> FAIL

--- RESTFulSense.Tests/Services/Foundations/Forms/FormServiceTests.Validations.AddStreams.cs
+++ RESTFulSense.Tests/Services/Foundations/Forms/FormServiceTests.Validations.AddStreams.cs
@@ -61,5 +61,62 @@ namespace RESTFulSense.Tests.Services.Foundations.Forms
 
             this.multipartFormDataContentBroker.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(data: null)]
+        [InlineData(data: "")]
+        [InlineData(data: "   ")]
+        public void ShouldThrowFormValidationExceptionOnAddStreamContentWithFileNameIfArgumentsInvalid(
+            string invalidInput)
+        {
+            // given
+            MultipartFormDataContent nullMultipartFormDataContent = CreateNullMultipartFormDataContent();
+            Stream nullContent = null;
+            string invalidName = invalidInput;
+            string invalidFileName = invalidInput;
+
+            var invalidFormArgumentException =
+                new InvalidFormArgumentException();
+
+            invalidFormArgumentException.AddData(
+                key: "MultipartFormDataContent",
+                values: "Form data content is required");
+
+            invalidFormArgumentException.AddData(
+                key: "StreamContent",
+                values: "Content is required");
+
+            invalidFormArgumentException.AddData(
+                key: "Name",
+                values: "Text is required");
+
+            invalidFormArgumentException.AddData(
+                key: "FileName",
+                values: "Text is required");
+
+            var expectedFormValidationException =
+                new FormValidationException(invalidFormArgumentException);
+
+            // when
+            Action addByteContentAction =
+                () => formService.AddStreamContent(
+                    multipartFormDataContent: nullMultipartFormDataContent,
+                    content: nullContent,
+                    name: invalidName,
+                    fileName: invalidFileName);
+
+            FormValidationException actualFormValidationException =
+                Assert.Throws<FormValidationException>(addByteContentAction);
+
+            // then
+            actualFormValidationException.Should()
+                .BeEquivalentTo(expectedFormValidationException);
+
+            this.multipartFormDataContentBroker.Verify(broker =>
+                broker.AddStreamContent(nullMultipartFormDataContent, nullContent, invalidName, invalidFileName),
+                    Times.Never);
+
+            this.multipartFormDataContentBroker.VerifyNoOtherCalls();
+        }
     }
 }
