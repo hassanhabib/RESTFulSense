c8fdca9b7e3469ad044b82d9dd817fd661694dac
Christo du Toit <christo@dutoit.co.uk>
2023-05-03 21:35:42 +0100

    ShouldBuildFormModel -> PASS

--- RESTFulSense/Services/Orchestrations/Forms/FormOrchestrationService.cs
+++ RESTFulSense/Services/Orchestrations/Forms/FormOrchestrationService.cs
@@ -2,6 +2,9 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System.Collections.Generic;
+using System.IO;
+using RESTFulSense.Models.Attributes;
 using RESTFulSense.Models.Orchestrations.Forms;
 using RESTFulSense.Services.Foundations.Attributes;
 using RESTFulSense.Services.Foundations.Forms;
@@ -25,7 +28,113 @@ namespace RESTFulSense.Services.Orchestrations.Forms
             this.formService = formService;
         }
 
-        public FormModel BuildFormModel(FormModel formModel) =>
-            throw new System.NotImplementedException();
+        public FormModel BuildFormModel(FormModel formModel)
+        {
+            var nameToFileNameMap = CreateNameToFileNameMap(formModel);
+            AddStringContents(formModel);
+            AddByteArrayContents(formModel, nameToFileNameMap);
+            AddStreamContents(formModel, nameToFileNameMap);
+
+            return formModel;
+        }
+
+        private Dictionary<string, string> CreateNameToFileNameMap(FormModel formModel)
+        {
+            var nameToFileNameMap = new Dictionary<string, string>();
+
+            foreach (var property in formModel.Properties)
+            {
+                RESTFulFileNameAttribute fileNameAttribute =
+                    this.attributeService.RetrieveAttribute<RESTFulFileNameAttribute>(property);
+
+                if (fileNameAttribute != null)
+                {
+                    object propertyValue =
+                        this.valueService.RetrievePropertyValue(formModel.Object, property);
+
+                    string fileName = (string)propertyValue;
+                    nameToFileNameMap[fileNameAttribute.Name] = fileName;
+                }
+            }
+
+            return nameToFileNameMap;
+        }
+
+        private void AddStringContents(FormModel formModel)
+        {
+            foreach (var property in formModel.Properties)
+            {
+                RESTFulStringContentAttribute attribute =
+                    this.attributeService.RetrieveAttribute<RESTFulStringContentAttribute>(property);
+
+                if (attribute != null)
+                {
+                    string value = (string)this.valueService.RetrievePropertyValue(formModel.Object, property);
+                    this.formService.AddStringContent(formModel.MultipartFormDataContent, value, attribute.Name);
+                }
+            }
+        }
+
+        private void AddByteArrayContents(FormModel formModel, Dictionary<string, string> fileNames)
+        {
+            foreach (var property in formModel.Properties)
+            {
+                RESTFulByteArrayContentAttribute attribute =
+                    this.attributeService.RetrieveAttribute<RESTFulByteArrayContentAttribute>(property);
+
+                if (attribute != null)
+                {
+                    byte[] value = (byte[])this.valueService.RetrievePropertyValue(formModel.Object, property);
+                    string name = attribute.Name;
+
+                    if (fileNames.ContainsKey(name))
+                    {
+                        this.formService.AddByteArrayContent(
+                            formModel.MultipartFormDataContent,
+                            value,
+                            attribute.Name,
+                            fileNames[name]);
+                    }
+                    else
+                    {
+                        this.formService.AddByteArrayContent(
+                            formModel.MultipartFormDataContent,
+                            value,
+                            attribute.Name);
+                    }
+                }
+            }
+        }
+
+        private void AddStreamContents(FormModel formModel, Dictionary<string, string> fileNames)
+        {
+            foreach (var property in formModel.Properties)
+            {
+                RESTFulStreamContentAttribute attribute =
+                    this.attributeService.RetrieveAttribute<RESTFulStreamContentAttribute>(property);
+
+                if (attribute != null)
+                {
+                    Stream value = (Stream)this.valueService.RetrievePropertyValue(formModel.Object, property);
+                    string name = attribute.Name;
+
+                    if (fileNames.ContainsKey(name))
+                    {
+                        this.formService.AddStreamContent(
+                            formModel.MultipartFormDataContent,
+                            value,
+                            attribute.Name,
+                            fileNames[name]);
+                    }
+                    else
+                    {
+                        this.formService.AddStreamContent(
+                            formModel.MultipartFormDataContent,
+                            value,
+                            attribute.Name);
+                    }
+                }
+            }
+        }
     }
 }
