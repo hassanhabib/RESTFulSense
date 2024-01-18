c88e1d939949b410511aa26289d41720a733f893
Greg Hays <glhays.dev@gmail.com>
2024-01-17 18:05:16 -0800

    ACCEPTANCE: Init Project
    Close: # 159

---
+++ RESTFulSense.Tests.Acceptance/DeleteMe.cs
@@ -0,0 +1,15 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Xunit;
+
+namespace RESTFulSense.Tests.Acceptance
+{
+    public class DeleteMe
+    {
+        [Fact]
+        private void ShouldBeTrue() =>
+            Assert.True(condition: true);
+    }
+}
