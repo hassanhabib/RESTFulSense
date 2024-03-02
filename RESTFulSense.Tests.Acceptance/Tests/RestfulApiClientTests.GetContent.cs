a72a482767cca05f32225b630b137259d778c5ef
Greg Hays <glhays.dev@gmail.com>
2024-03-01 16:26:29 -0800

    ACCEPTANCE: ApiClientTests GetContent

---
+++ RESTFulSense.Tests.Acceptance/Tests/RestfulApiClientTests.GetContent.cs
@@ -0,0 +1,109 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using RESTFulSense.Tests.Acceptance.Models;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using Xunit;
+
+namespace RESTFulSense.Tests.Acceptance.Tests
+{
+    public partial class RestfulApiClientTests
+    {
+
+        [Fact]
+        private async Task ShouldGetContentWithDeserializationFunctionAsync()
+        {
+            // given
+            TEntity expectedReponseEntity = GetRandomTEntity();
+
+            wiremockServer.Given(Request.Create()
+                .WithPath(relativeUrl)
+                .UsingGet())
+                    .RespondWith(Response.Create()
+                    .WithStatusCode(200)
+                    .WithBodyAsJson(expectedReponseEntity));
+
+            // when
+            TEntity actualResponseEntity =
+                await this.restfulApiClient.GetContentAsync<TEntity>(
+                    relativeUrl,
+                    deserializationFunction: DeserializationContentFunction);
+
+            // then
+            actualResponseEntity.Should().BeEquivalentTo(expectedReponseEntity);
+        }
+
+        [Fact]
+        private async Task ShouldCancelGetContentDeserializationIfCancellationInvokedAsync()
+        {
+            // given
+            TEntity someContent = GetRandomTEntity();
+            var expectedCanceledTaskException = new TaskCanceledException();
+            var taskCancelInvoked = new CancellationToken(canceled: true);
+
+            wiremockServer.Given(Request.Create()
+                .WithPath(relativeUrl)
+                .UsingGet())
+                    .RespondWith(Response.Create()
+                        .WithBodyAsJson(someContent));
+
+            // when
+            var actualCanceledTask =
+                await Assert.ThrowsAsync<TaskCanceledException>(async () =>
+                    await this.restfulApiClient.GetContentAsync<TEntity>(
+                        relativeUrl,
+                        cancellationToken: taskCancelInvoked,
+                        deserializationFunction: DeserializationContentFunction));
+
+            // then
+            actualCanceledTask.Should().BeEquivalentTo(expectedCanceledTaskException);
+        }
+
+        [Fact]
+        private async Task ShouldReturnStringOnGetContentStringAsync()
+        {
+            // given
+            string stringContent = CreateRandomContent();
+
+            wiremockServer.Given(Request.Create()
+                .WithPath(relativeUrl)
+                .UsingGet())
+                    .RespondWith(Response.Create()
+                        .WithBody(stringContent));
+
+            // when
+            var actualContentResponse =
+                await this.restfulApiClient.GetContentStringAsync(relativeUrl);
+
+            // then
+            actualContentResponse.Should().BeEquivalentTo(stringContent);
+        }
+
+        [Fact]
+        private async Task ShouldReturnStreamOnGetContentStreamAsync()
+        {
+            // given
+            string stringContent = CreateRandomContent();
+
+            wiremockServer.Given(Request.Create()
+                .WithPath(relativeUrl)
+                .UsingGet())
+                    .RespondWith(Response.Create()
+                    .WithBody(stringContent));
+
+            // when
+            var expectedContentStream =
+                await this.restfulApiClient.GetContentStreamAsync(relativeUrl);
+
+            var actualReadStream = await ReadStreamToEndAsync(expectedContentStream);
+
+            // then
+            actualReadStream.Should().BeEquivalentTo(stringContent);
+        }
+    }
+}
