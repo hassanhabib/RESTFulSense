2067501c3864caf0fa8cb54bd4baa562938a46cd
Greg Hays <glhays.dev@gmail.com>
2024-03-08 13:57:21 -0800

    ACCEPTANCE: RestfulSenseWebAssemblyApiClient PutContent

---
+++ RESTFulSense.WebAssenbly.Tests.Acceptance/Tests/RestfulSenseWebAssemblyApiClient.PutContent.cs
@@ -0,0 +1,209 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Newtonsoft.Json;
+using RESTFulSense.WebAssenbly.Tests.Acceptance.Models;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using Xunit;
+
+namespace RESTFulSense.WebAssenbly.Tests.Acceptance.Tests
+{
+    public partial class RestfulSenseWebAssemblyApiClient
+    {
+        [Fact]
+        private async Task ShouldPutContentReturnsExpectedWithSerializedAndDeserializedContentAsync()
+        {
+            // given
+            TEntity randomTEntity = GetRandomTEntity();
+            TEntity expectedTEntity = randomTEntity;
+            string mediaType = "text/json";
+            bool ignoreDefaultValues = false;
+
+            this.wiremockServer
+                .Given(Request.Create()
+                    .WithPath(relativeUrl)
+                    .UsingPut())
+                        .RespondWith(Response.Create()
+                            .WithStatusCode(200)
+                            .WithBodyAsJson(randomTEntity));
+
+            // when
+            TEntity actualTEntity =
+                await this.restfulApiClient.PutContentAsync<TEntity>(
+                    relativeUrl: relativeUrl,
+                    content: randomTEntity,
+                    mediaType: mediaType,
+                    ignoreDefaultValues: ignoreDefaultValues,
+                    serializationFunction: SerializationContentFunction<TEntity>,
+                    deserializationFunction: DeserializationContentFunction);
+
+            // then
+            actualTEntity.Should().BeEquivalentTo(expectedTEntity);
+        }
+
+        [Fact]
+        private async Task ShouldCancelTaskOnPutContentReturnsContentIfCancellationInvokedAsync()
+        {
+            // given
+            TEntity randomTEntity = GetRandomTEntity();
+            string mediaType = "text/json";
+            bool ignoreDefaultValues = false;
+            var expectedCanceledTaskException = new TaskCanceledException();
+            var taskCancelInvoked = new CancellationToken(canceled: true);
+
+            wiremockServer.Given(Request.Create()
+                .WithPath(relativeUrl)
+                .UsingPut())
+                    .RespondWith(Response.Create()
+                        .WithBodyAsJson(randomTEntity));
+
+            // when
+            TaskCanceledException actualCanceledTask =
+                await Assert.ThrowsAsync<TaskCanceledException>(async () =>
+                    await this.restfulApiClient.PutContentAsync<TEntity>(
+                        relativeUrl: relativeUrl,
+                        content: randomTEntity,
+                        cancellationToken: taskCancelInvoked,
+                        mediaType: mediaType,
+                        ignoreDefaultValues: ignoreDefaultValues,
+                        serializationFunction: SerializationContentFunction<TEntity>,
+                        deserializationFunction: DeserializationContentFunction));
+
+            // then
+            actualCanceledTask.Should().BeEquivalentTo(expectedCanceledTaskException);
+        }
+
+        [Fact]
+        private async Task ShouldPutContentWithTContentReturnsTResultAsync()
+        {
+            // given
+            TEntity randomTEntity = GetRandomTEntity();
+            TEntity expectedTEntity = randomTEntity;
+            string expectedBody = JsonConvert.SerializeObject(randomTEntity);
+            string mediaType = "text/json";
+            bool ignoreDefaultValues = false;
+
+            this.wiremockServer.Given(
+                Request.Create()
+                    .WithPath(relativeUrl)
+                    .UsingPut())
+                        .RespondWith(
+                            Response.Create()
+                                .WithStatusCode(200)
+                                .WithHeader("Content-Type", mediaType)
+                                .WithBody(expectedBody));
+
+            // when
+            TEntity actualTEntity =
+                await this.restfulApiClient.PutContentAsync<TEntity, TEntity>(
+                    relativeUrl: relativeUrl,
+                    content: randomTEntity,
+                    mediaType: mediaType,
+                    ignoreDefaultValues: ignoreDefaultValues,
+                    serializationFunction: SerializationContentFunction<TEntity>,
+                    deserializationFunction: DeserializationContentFunction);
+
+            // then
+            actualTEntity.Should().BeEquivalentTo(expectedTEntity);
+        }
+
+        [Fact]
+        private async Task ShouldPutContentWithTContentReturnsTResultWithCancellationTokenAsync()
+        {
+            // given
+            TEntity randomTEntity = GetRandomTEntity();
+            TEntity expectedTEntity = randomTEntity;
+            string expectedBody = JsonConvert.SerializeObject(randomTEntity);
+            CancellationToken cancellationToken =
+                new CancellationToken(canceled: false);
+
+            string mediaType = "text/json";
+            bool ignoreDefaultValues = false;
+
+            this.wiremockServer.Given(
+                Request.Create()
+                    .WithPath(relativeUrl)
+                    .UsingPut())
+                        .RespondWith(
+                            Response.Create()
+                                .WithStatusCode(200)
+                                .WithHeader("Content-Type", mediaType)
+                                .WithBody(expectedBody));
+
+            // when
+            TEntity actualTEntity =
+                await this.restfulApiClient.PutContentAsync<TEntity, TEntity>(
+                    relativeUrl: relativeUrl,
+                    content: randomTEntity,
+                    cancellationToken: cancellationToken,
+                    mediaType: mediaType,
+                    ignoreDefaultValues: ignoreDefaultValues,
+                    serializationFunction: SerializationContentFunction,
+                    deserializationFunction: DeserializationContentFunction);
+
+            // then
+            actualTEntity.Should().BeEquivalentTo(expectedTEntity);
+        }
+
+        [Fact]
+        private async Task ShouldPutContentReturnsContentWithDeserializationAsync()
+        {
+            // given
+            TEntity randomTEntity = GetRandomTEntity();
+            TEntity expectedTEntity = randomTEntity;
+            string expectedBody = JsonConvert.SerializeObject(randomTEntity);
+
+            this.wiremockServer.Given(
+                Request.Create()
+                    .WithPath(relativeUrl)
+                    .UsingPut())
+                        .RespondWith(
+                            Response.Create()
+                                .WithStatusCode(200)
+                                .WithBody(expectedBody));
+
+            // when
+            TEntity actualTEntity =
+                await this.restfulApiClient.PutContentAsync<TEntity>(
+                    relativeUrl: relativeUrl,
+                    deserializationFunction: DeserializationContentFunction);
+
+            // then
+            actualTEntity.Should().BeEquivalentTo(expectedTEntity);
+        }
+
+        [Fact]
+        private async Task ShouldPutContentReturnsContentWithCancellationTokenDeserializationAsync()
+        {
+            // given
+            TEntity randomTEntity = GetRandomTEntity();
+            TEntity expectedTEntity = randomTEntity;
+            string expectedBody = JsonConvert.SerializeObject(randomTEntity);
+            var cancellationToken = new CancellationToken(canceled: false);
+
+            this.wiremockServer.Given(
+                Request.Create()
+                    .WithPath(relativeUrl)
+                    .UsingPut())
+                        .RespondWith(
+                            Response.Create()
+                                .WithStatusCode(200)
+                                .WithBody(expectedBody));
+
+            // when
+            TEntity actualTEntity =
+                await this.restfulApiClient.PutContentAsync<TEntity>(
+                    relativeUrl: relativeUrl,
+                    cancellationToken: cancellationToken,
+                    deserializationFunction: DeserializationContentFunction);
+
+            // then
+            actualTEntity.Should().BeEquivalentTo(expectedTEntity);
+        }
+    }
+}
