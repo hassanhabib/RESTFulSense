7fe123a3bd5eb946ceb4d678af001b1940f7e606
Hassan Habib <hassanhabib@live.com>
2024-09-19 00:16:37 -0700

    CLIENTS: Add Action Result Extensions

---
+++ RESTFulSense/Clients/Extensions/ActionResultExtension.cs
@@ -0,0 +1,20 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTFulSense.Clients.Extensions
+{
+    public static class ActionResultExtension
+    {
+        public static void ShouldBeEquivalentTo<T>(
+            this ActionResult<T> actualActionResult,
+            ActionResult<T> expectedActionResult)
+        {
+            (actualActionResult.Result as ObjectResult).Should().BeEquivalentTo(
+                expectedActionResult.Result as ObjectResult);
+        }
+    }
+}
