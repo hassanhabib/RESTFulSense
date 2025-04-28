8a10cebc674d8daa6888ba50c22212d638ed96b2
ZafarUrakov <zafarurakov@outlook.com>
2025-04-28 11:01:03 +0800

    ShouldNotSetResultWhenHeaderMatches -> FAIL

---
+++ RESTFulSense.Tests/Models/Attributes/RESTFulRestrictHeaderAttributes/RESTFulRestrictHeaderAttributeTests.Logic.cs
@@ -0,0 +1,52 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using RESTFulSense.Models.Attributes;
+using Xunit;
+
+namespace RESTFulSense.Tests.Models.Attributes.RESTFulRestrictHeaderAttributes
+{
+    public partial class RESTFulRestrictHeaderAttributeTests
+    {
+        [Fact]
+        public void ShouldNotSetResultWhenHeaderMatches()
+        {
+            // give
+            string randomName = GetRandomString();
+            string inputName = randomName;
+            string randomValue = GetRandomString();
+            string inputValue = randomValue;
+
+            var expectedRestfulRestrictHeaderAttribute =
+                new RESTFulRestrictHeaderAttribute
+                {
+                    Name = inputName,
+                    Value = inputValue
+                };
+
+            var defaultHttpContext = new DefaultHttpContext();
+
+            defaultHttpContext.Request.Headers[key: inputName] =
+                inputValue;
+
+            ActionContext inputActionContext =
+                CreateRandomActionContext(defaultHttpContext);
+
+            ActionExecutingContext inputActionExecutingContext =
+                CreateRandomActionExecutingContext(
+                    inputActionContext);
+
+            // when
+            expectedRestfulRestrictHeaderAttribute
+                .OnActionExecuting(inputActionExecutingContext);
+
+            // then
+            inputActionExecutingContext.Result.Should().BeNull();
+        }
+    }
+}
---
+++ RESTFulSense.Tests/Models/Attributes/RESTFulRestrictHeaderAttributes/RESTFulRestrictHeaderAttributeTests.cs
@@ -0,0 +1,47 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Tynamix.ObjectFiller;
+
+namespace RESTFulSense.Tests.Models.Attributes.RESTFulRestrictHeaderAttributes
+{
+    public partial class RESTFulRestrictHeaderAttributeTests
+    {
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+
+        private static ActionContext CreateRandomActionContext(HttpContext httpContext)
+        {
+            var randomRouteData = new RouteData();
+            var randomActionDescriptor = new ControllerActionDescriptor();
+
+            return new ActionContext
+            {
+                HttpContext = httpContext,
+                RouteData = randomRouteData,
+                ActionDescriptor = randomActionDescriptor
+            };
+        }
+
+        private static ActionExecutingContext CreateRandomActionExecutingContext(
+            ActionContext actionContext)
+        {
+            var randomFilters = new List<IFilterMetadata>();
+            var randomActionArguments = new Dictionary<string, object>();
+            object nullController = null;
+
+            return new ActionExecutingContext(
+                actionContext,
+                randomFilters,
+                randomActionArguments,
+                nullController);
+        }
+    }
+}
