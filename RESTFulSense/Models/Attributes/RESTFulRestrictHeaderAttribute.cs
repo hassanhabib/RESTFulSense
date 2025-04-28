9c9a5e5a2bee142dff0fb713560e91ed0b104ca4
ZafarUrakov <zafarurakov@outlook.com>
2025-04-28 11:01:07 +0800

    ShouldNotSetResultWhenHeaderMatches -> PASS

---
+++ RESTFulSense/Models/Attributes/RESTFulRestrictHeaderAttribute.cs
@@ -0,0 +1,20 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace RESTFulSense.Models.Attributes
+{
+    public sealed class RESTFulRestrictHeaderAttribute : ActionFilterAttribute
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+        }
+    }
+}
